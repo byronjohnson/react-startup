123ca1e0732322f9a0a925cbf82ffa6d9da81b83
Byron <bjohn3333@gmail.com>
2021-01-01 17:24:52 -0500

    Include Axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 import "semantic-ui-css/semantic.min.css";
 
+
 class App extends React.Component {
 
-    onSearchSubmit(term){
+    async onSearchSubmit(term){
         console.log('here is the term caught in App:');
         console.log(term);
+
+        const unsplashEndpoint = 'https://api.unsplash.com/search/photos';
+
+        const response = await axios.get(
+            unsplashEndpoint,
+            {
+                params: {
+                    query: term
+                },
+                headers: {
+                    Authorization: 'Client-ID baAsnw73DRkWDm1BAgMWnzjpyQogI5qSULI51HWTeNc'
+                }
+            }
+        );
+
+        console.log(response.data);
     }
 
     render(){
