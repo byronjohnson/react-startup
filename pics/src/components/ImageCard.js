01cbc99797b2cef9d304b9f388cfbca0b4662af0
Byron <bjohn3333@gmail.com>
2021-02-22 23:44:24 -0500

    Create videos project

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -5,7 +5,11 @@ class ImageCard extends React.Component {
     constructor(props){
         super(props);
 
+        this.state = { spans: 0 }
+
         this.imageRef = React.createRef();
+
+        
     }
 
     componentDidMount(){
@@ -13,11 +17,19 @@ class ImageCard extends React.Component {
     }
 
     setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({
+            spans: spans
+        });
+
+        console.log(height);
     };
 
     render(){
-        return ( <div>
+        return ( 
+        <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
             <img ref={this.imageRef} src={this.props.image.urls.thumb} alt={this.props.image.description} />
             <div>{this.props.image.user.first_name}  {this.props.image.user.last_name}</div>
         </div>
